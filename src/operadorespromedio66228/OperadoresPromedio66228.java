639200ccc40d3aff810126965eaa633c479ffc47
End User <End User@DESKTOP-62AKJQO>
2021-02-28 17:23:27 -0600

    Link Git

---
+++ src/operadorespromedio66228/OperadoresPromedio66228.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadorespromedio66228;
+
+/**
+ *
+ * @author End User
+ */
+public class OperadoresPromedio66228 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double num1 = 10;
+        double num2 = 3;
+        double num3 = 7;
+        double num4 = 8;
+        double num5 = 6;
+        double promedio = ((num1 + num2 + num3 + num4 + num5) /5);
+        System.out.print("El promedio es " + promedio + "\n");
+    }
+    
+}
